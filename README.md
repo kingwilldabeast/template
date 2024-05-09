1b7d90a4fc2b955d164b724035acea7043880498
Will Isenberg <41377997+kingwilldabeast@users.noreply.github.com>
2024-05-09 17:33:21 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Template of common code we learned in class
+
+Feel free to add other examples, especially for CSS and flex, and push your changes to the main template
---
+++ script.js
@@ -0,0 +1,37 @@
+/*-------------------------------- Constants --------------------------------*/
+const myArray = []
+const myObject = {name: "Bill", age: 24, alive: true}
+
+/*---------------------------- Variables (state) ----------------------------*/
+let myNumber = 1
+let myBoolean = true
+let myString = "hello"
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+const buttonElement = document.querySelector('#myButtonID')
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+function myFunction(myArgument) {
+    return myArgument + 1
+}
+
+async function myAPIFunction() {
+
+        let apiKey = '123456'
+        let input = "Han Solo"
+      
+        let response = await axios.get(
+            `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${input}&days=7&aqi=no&alerts=no`
+        )
+
+}
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+buttonElement.addEventListener('click', myFunction)
